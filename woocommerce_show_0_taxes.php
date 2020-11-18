34531e6de97382ac1ed07bf6dcc2d2b4b004bc22
ander <apijoan@sammic.com>
2020-11-18 17:31:01 +0100

    Comment

--- woocommerce_show_0_taxes.php
+++ woocommerce_show_0_taxes.php
@@ -1,12 +1,15 @@
 <?php
 /*
-Plugin Name: Show 0 taxes in Woocommerce
-Plugin URI: https://github.com/AnderPijoan/woocommerce_show_0_taxes
-Description: Shows taxes although their value is 0
-Author: Ander Pijoan
-Version: 1.0
-Author URI: https://github.com/AnderPijoan
-License: MIT
+* Show 0 taxes in Woocommerce
+*
+* @wordpress-plugin
+* Plugin Name: Show 0 taxes in Woocommerce
+* Plugin URI: https://github.com/AnderPijoan/woocommerce_show_0_taxes
+* Description: Shows taxes although their value is 0
+* Author: Ander Pijoan
+* Version: 1.0
+* Author URI: https://github.com/AnderPijoan
+* License: MIT
 */
 
 add_filter( 'woocommerce_cart_hide_zero_taxes', '__return_false' ); 
